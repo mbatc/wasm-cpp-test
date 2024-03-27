a2c4dd2553967ab05eaca050d7459c64a8c99a02
Michael Batchelor <mickbatch98@gmail.com>
2024-03-27 23:48:18 +1000

    Update site to use roslibjs

--- build-jupyterlite.sh
+++ build-jupyterlite.sh
@@ -1,5 +1,6 @@
 
 EMFORGE_ENV_NAME="$(get_conf_var "emforge-env-name")"
+WASM_CPP_DIR="$(get_conf_var "wasm-cpp-dir")"
 
 micromamba activate $EMFORGE_ENV_NAME
 
@@ -13,5 +14,12 @@ jupyter lite build --XeusPythonEnv.empack_config=empack_config.yaml
 popd
 
 mv jupyterlite/_output dist
+
+# Download roslib and replace 'window' with '_window'
+ROSLIB_SRC="$(curl "https://cdn.jsdelivr.net/npm/roslib@1/build/roslib.js")"
+ROSLIB_SRC=${ROSLIB_SRC//window/_window}
+echo "$ROSLIB_SRC"> dist/roslib.js
+
+cp "$WASM_CPP_DIR/wasm_cpp/src/pre.js" "dist/pre.js"
 
 micromamba deactivate
--- build.sh
+++ build.sh
@@ -8,8 +8,9 @@ BUILD_NUMBER="$(get_conf_var "build-number")"
 WASM_CPP_DIR="$(get_conf_var "wasm-cpp-dir")"
 EMFORGE_DIR="$(get_conf_var "emscripten-forge-dir")"
 
-write_template "./recipes/ros-humble-rmw-wasm-cpp/recipe.yaml.in" "./recipes/ros-humble-rmw-wasm-cpp/recipe.yaml"
-write_template "./recipes/ros-humble-wasm-cpp/recipe.yaml.in"     "./recipes/ros-humble-wasm-cpp/recipe.yaml"
+write_template "./recipes/ros-humble-wasm-cpp/recipe.yaml.in"      "./recipes/ros-humble-wasm-cpp/recipe.yaml"
+write_template "./recipes/ros-humble-rmw-wasm-cpp/recipe.yaml.in"  "./recipes/ros-humble-rmw-wasm-cpp/recipe.yaml"
+# write_template "./recipes/ros-humble-wasm-cpp-test/recipe.yaml.in" "./recipes/ros-humble-wasm-cpp-test/recipe.yaml"
 
 micromamba activate "$(get_conf_var "emforge-env-name")"
 boa build recipes --target-platform=emscripten-wasm32 -m $EMFORGE_DIR/conda_build_config.yaml
